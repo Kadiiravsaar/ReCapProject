6dd016cb744f51bb2680ea7acf91558e3090d7da
kadiiravsaar <kadiiravsar@gmail.com>
2023-05-11 15:29:52 +0300

    Customer added

---
+++ Buisness/Abstract/ICustomerService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Ultities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisness.Abstract
+{
+    public interface ICustomerService
+    {
+        IResult Add(Customer customer);
+        IResult Update(Customer customer);
+        IResult Delete(Customer customer);
+        IDataResult<List<Customer>> GetAll();
+        IDataResult<Customer> GetByCustomerId(int customerId);
+    }
+}
---
+++ Buisness/Concrete/CustomerManager.cs
@@ -0,0 +1,53 @@
+﻿using Buisness.Abstract;
+using Core.Ultities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisness.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        private readonly ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public IResult Add(Customer customer)
+        {
+            _customerDal.Add(customer);
+            return new SuccessResult("Müşteri Eklendi");
+        }
+
+        public IResult Delete(Customer customer)
+        {
+            _customerDal.Delete(customer);
+            return new SuccessResult("Müşteri silindi.");
+        }
+
+        public IDataResult<List<Customer>> GetAll()
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll());
+        }
+
+        public IDataResult<Customer> GetByCustomerId(int customerId)
+        {
+            return new SuccessDataResult<Customer>(_customerDal.GetById(x=>x.CustomerId == customerId),"Getirildi");
+
+        }
+
+        public IResult Update(Customer customer)
+        {
+            _customerDal.Update(customer);
+            return new SuccessResult("Müşteri güncellendi.");
+        }
+    }
+}
