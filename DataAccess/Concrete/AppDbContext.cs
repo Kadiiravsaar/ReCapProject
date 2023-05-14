ed130bc1f21a2ef7dfade6d438de6a873092810b
kadiiravsaar <kadiiravsar@gmail.com>
2023-05-14 23:53:21 +0300

    ..

--- DataAccess/Concrete/AppDbContext.cs
+++ DataAccess/Concrete/AppDbContext.cs
@@ -20,5 +20,6 @@ namespace DataAccess.Concrete
         public DbSet<Brand> Brands { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
     }
 }
