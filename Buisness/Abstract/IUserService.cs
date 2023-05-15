c9427a302f7c7b29433bc224fd303cc5d34980ed
kadiiravsaar <kadiiravsar@gmail.com>
2023-05-15 19:32:32 +0300

    User Added

---
+++ Buisness/Abstract/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Ultities.Results;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisness.Abstract
+{
+    public interface IUserService
+    {
+        IResult Add(User user); 
+    }
+}
---
+++ Buisness/Concrete/UserManager.cs
@@ -0,0 +1,28 @@
+﻿using Buisness.Abstract;
+using Core.Ultities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buisness.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private readonly IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public IResult Add(User user)
+        {
+            _userDal.Add(user);
+            return new SuccessResult("Kullanıcı Eklendi");
+        }
+    }
+}
