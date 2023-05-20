3e21e08bc78872e6b906cc0975b7bd4be0c3e323
kadiiravsaar <kadiiravsar@gmail.com>
2023-05-20 18:06:51 +0300

    UsersControllerAdded

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Buisness.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("adduser")]
+        public IActionResult AddUser(User user)
+        {
+            var result = _userService.Add(user);
+            if (result.Success)
+            {
+                return Ok(result);
+
+            }
+            return BadRequest();
+        }
+    }
+}
