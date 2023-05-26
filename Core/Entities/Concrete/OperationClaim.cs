2c77bca69af8be55393fc51b8bf40da33b7cc751
Kadiiravsaar <kadiiravsar@gmail.com>
2023-05-26 16:21:12 +0300

    User class revize UserOperationClaim Added OperationClaim added

---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class OperationClaim
+    {
+        public int OperationClaimId { get; set; }
+        public string Name { get; set; } //bu claimler operasyon bazlı veya 'admin','editor' vs şeklinde de olabilir.
+    }
+}
---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordSalt { get; set; } // salt demek haslenen verilin sonuna kendimiz bi veri daha ekleiyoruz tuzluyoruz demek
+        public byte[] PasswordHash { get; set; }//şifre hash'lerken kendim tuzlama yapıyorum, tekrar şifreyi verify edeceğim zaman bu salt değerine ihtiyacım var.
+        public bool Status { get; set; }
+    }
+}
